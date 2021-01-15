bc7ea81dd06bfb68c101a7659c7f7aba44c62067
jeonseun <jeonseun0000@gmail.com>
2021-01-15 19:11:42 +0900

    Initial commit

---
+++ src/main/java/team/univ/magic_conch/MagicConchApplication.java
@@ -0,0 +1,13 @@
+package team.univ.magic_conch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicConchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicConchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/univ/magic_conch/MagicConchApplicationTests.java
@@ -0,0 +1,13 @@
+package team.univ.magic_conch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicConchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
