6c84016ff42d3b44a1ca00e7ba3c84f7aaacf928
HaJooHyun <mangchhe@naver.com>
2021-02-15 23:56:15 +0900

    Update index HTML, Js, AnswerController [홈에서 팔로우 질문 답글 보기 구현]

--- src/main/java/team/univ/magic_conch/answer/AnswerController.java
+++ src/main/java/team/univ/magic_conch/answer/AnswerController.java
@@ -2,10 +2,14 @@ package team.univ.magic_conch.answer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import team.univ.magic_conch.answer.dto.AnswerDTO;
 import team.univ.magic_conch.answer.dto.CreateAnswerDTO;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,9 +24,22 @@ public class AnswerController {
      */
     @PostMapping("/answer")
     @ResponseBody
-    public AnswerDTO answer(CreateAnswerDTO createAnswerDTO) {
+    public AnswerDTO createAnswer(CreateAnswerDTO createAnswerDTO) {
 
         return  answerService.createAnswer(createAnswerDTO);
+
+    }
+
+    /**
+     * 해당 질문에 대한 답변 전달
+     * @param questionId
+     * @return 해당 질문에 대한 답변 목록
+     */
+    @GetMapping("/answer/info")
+    @ResponseBody
+    public List<AnswerDTO> answer(@RequestParam(value = "question") Long questionId){
+
+        return answerService.answer(questionId);
 
     }
 
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,6 +2,51 @@ $(document).ready(function (){
     questionInfo();
 });
 
+function clickAnswerBtn(questionId, username){
+    $.ajax({
+        url: '/answer/info',
+        type: 'GET',
+        data: {'question' : questionId},
+        dataType: 'json',
+        success: function (data) {
+
+            let question = document.getElementById('answer-wrapper-' + questionId);
+
+            question.innerHTML =
+                "<div class=\"text-end mt-4\">\n" +
+                "    <div class=\"form-outline\">\n" +
+                "        <div style=\"display: none\" id=\"question\">" + questionId +"</div>\n" +
+                "        <div style=\"display: none\" id=\"user\"> + username +</div>\n" +
+                "        <textarea class=\"form-control\" id=\"answerContent\" rows=\"4\" name=\"content\"></textarea>\n" +
+                "        <label class=\"form-label\" for=\"answerContent\">답변 내용</label>\n" +
+                "    </div>\n" +
+                "    <button type=\"button\" class=\"mt-2 mb-2 btn btn-outline-primary\" onclick=\"clickAnswerBtn()\">작성</button>\n" +
+                "</div>";
+
+            for (let i = 0; i < data.length; i++) {
+                question.innerHTML +=
+                    "<div class=\"answer\">\n" +
+                    "    <!-- 답변 보기 -->\n" +
+                    "    <div" +
+                    "        <hr>\n" +
+                    "        <div class=\"d-flex align-items-center\ value=" + data[i].username + " onclick=\"clickAnswerProfile(this)\"s>\n" +
+                    "            <div class=\"d-flex flex-column ps-2\">\n" +
+                    "                <img src=" + data[i].profileImg + " \n" +
+                    "                     class=\"rounded-circle\"\n" +
+                    "                     height=\"35px\"\n" +
+                    "                     width=\"35px\">\n" +
+                    "                <div class=\"text-center\" style=\"font-size: 20px\">" + data[i].username + "</div>\n" +
+                    "            </div>\n" +
+                    "            <div class=\"ps-3\" style=\"word-break:break-all\">" + data[i].content + "</div>\n" +
+                    "        </div>\n" +
+                    "    </div>\n" +
+                    "</div>";
+            }
+        }, error: function (xhr) {
+        }
+    })
+}
+
 function questionInfo() {
 
     var params = jQuery('.form-data').serialize();
@@ -31,10 +76,12 @@ function questionInfo() {
                         "      <div>" + data.dtoList[i].content +"</div>\n" +
                         "    </div>\n" +
                         "    <div class='mt-5'>" +
-                        "      <button type=\"button\" class=\"answerBtn btn btn-outline-primary\">댓글 보기</button>" +
+                        "      <button type=\"button\" class=\"answerBtn btn btn-outline-primary\" onclick=clickAnswerBtn(" +data.dtoList[i].questionId + ",\"" + data.dtoList[i].username + "\")>댓글 보기</button>" +
                         "    </div>" +
                         "  </div>\n" +
-                        "</div>"
+                        "</div>" +
+                        "<div id=answer-wrapper-" + data.dtoList[i].questionId + ">" +
+                        "</div>";
                 }
                 else{
                     question.innerHTML +=
@@ -45,7 +92,7 @@ function questionInfo() {
                         "  </div>\n" +
                         "  <div class='d-flex'>\n" +
                         "    <div class='mt-5'>" +
-                        "      <button type=\"button\" class=\"answerBtn btn btn-outline-primary\">댓글 보기</button>" +
+                        "      <button type=\"button\" class=\"answerBtn btn btn-outline-primary\" onclick=clickAnswerBtn(" +data.dtoList[i].questionId + ",\"" + data.dtoList[i].username + "\")>댓글 보기</button>" +
                         "    </div>" +
                         "    <div onclick=\"location.href='/question/" + data.dtoList[i].questionId + "'\" class=\"balloon2\" style='padding: 10px; word-break: break-all; overflow: auto'>\n" +
                         "      <div class=\"d-flex justify-content-between\">\n" +
@@ -56,7 +103,9 @@ function questionInfo() {
                         "      <div>" + data.dtoList[i].content +"</div>\n" +
                         "    </div>\n" +
                         "  </div>\n" +
-                        "</div>"
+                        "</div>" +
+                        "<div id=answer-wrapper-" + data.dtoList[i].questionId + ">" +
+                        "</div>";
                 }
             }
             question.innerHTML +=
