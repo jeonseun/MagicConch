a293efd13e4fc5ab5d221796bb998ba9836f48ff
jeonseun <jeonseun0000@gmail.com>
2021-03-29 23:34:16 +0900

    fixed data.sql 대소문자 변경, application.yaml sql 파일 초기화 설정 추가 InitializerRunner.java 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO TAG(name, create_date, color, image) VALUES ( 'C', NOW(), '#39C0ED', 'c_icon.png' );
-INSERT INTO TAG(name, create_date, color, image) VALUES ( 'C++', NOW(), '#F93154', 'cpp_icon.png' );
-INSERT INTO TAG(name, create_date, color, image) VALUES ( 'Java', NOW(), '#1e90ff', 'java_icon.png' );
-INSERT INTO TAG(name, create_date, color, image) VALUES ( 'Python', NOW(), '#C964E2', 'python_icon.png' );
-INSERT INTO TAG(name, create_date, color, image) VALUES ( 'Spring', NOW(), '#00B74A', 'spring_icon.png' );
-INSERT INTO TAG(name, create_date, color, image) VALUES ( 'Javascript', NOW(), '#FFA900', 'js_icon.png' );
-INSERT INTO TAG(name, create_date, color, image) VALUES ( 'DEFAULT', NOW(), '#FBFBFB', 'default_icon.png' );
+INSERT INTO tag(name, create_date, color, image) VALUES ( 'C', NOW(), '#39C0ED', '/image/tag/c_icon.png' );
+INSERT INTO tag(name, create_date, color, image) VALUES ( 'C++', NOW(), '#F93154', '/image/tag/cpp_icon.png' );
+INSERT INTO tag(name, create_date, color, image) VALUES ( 'Java', NOW(), '#1e90ff', '/image/tag/java_icon.png' );
+INSERT INTO tag(name, create_date, color, image) VALUES ( 'Python', NOW(), '#C964E2', '/image/tag/python_icon.png' );
+INSERT INTO tag(name, create_date, color, image) VALUES ( 'Spring', NOW(), '#00B74A', '/image/tag/spring_icon.png' );
+INSERT INTO tag(name, create_date, color, image) VALUES ( 'Javascript', NOW(), '#FFA900', '/image/tag/js_icon.png' );
+INSERT INTO tag(name, create_date, color, image) VALUES ( 'DEFAULT', NOW(), '#FBFBFB', '/image/tag/default_icon.png' );
